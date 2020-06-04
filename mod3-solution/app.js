9c83cf66a34cd2964270e59becea73bfa0a04ee9
Neha <nehajagtap27@gmail.com>
2020-06-04 12:53:51 +0530

    new

--- mod3-solution/app.js
+++ mod3-solution/app.js
@@ -4,16 +4,17 @@
 angular.module('NarrowItDownApp',[])
 .controller('NarrowItDownController',NarrowItDownController)
 .service('MenuSearchService',MenuSearchService)
-.directive('foundItems',foundItems)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItems',FoundItems);
 
-
-function foundItems(){
+function FoundItems(){
   var ddo =
   {
-    templateUrl: 'foundItems.html';
+    restrict: 'E',
+    templateUrl: 'foundItems.html',
     scope:{
       foundItems: '<',
+      onEmpty: '<',
       onRemove: '&'
     },
     controller: NarrowItDownController,
@@ -24,48 +25,58 @@ function foundItems(){
   return ddo;
 }
 
-
-
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService)
 {
   var ctrl = this;
+  ctrl.shortName = "";
 
-  var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-
-  ctrl.getItems = function(searchTerm)
+  ctrl.matchedItems = function(searchTerm)
   {
-    promise.then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+    promise.then(function (items) {
+      if (items && items.length > 0)
+      {
+         ctrl.message = '';
+         ctrl.found = items;
+     } else
+      {
+        ctrl.message = 'Nothing found!';
+        ctrl.found = [];
+      }
+    });
   };
+  ctrl.removeMenuItem = function(itemIndex) {
+           ctrl.found.splice(itemIndex, 1);
+       }
+
 }
 
-MenuSearchService.$inject = ['$hhtp','ApiBasePath'];
+MenuSearchService.$inject = ['$http','ApiBasePath'];
 function MenuSearchService($http, ApiBasePath){
   var service = this;
 
   service.getMatchedMenuItems = function (searchTerm) {
    return $http({
      method: "GET",
-     url: (ApiBasePath + "/menu_items.json"),
-     params: {
-       category: searchTerm
-     }
-   }).then(function (result)
+     url: (ApiBasePath + "/menu_items.json")
+   }).then(function (response)
  {
-   var foundItems;
+   var foundItems = [];
 
+   for (var i = 0; i < response.data['menu_items'].length; i++)
+   {
+        if (searchTerm.length > 0 && response.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm) !== -1)
+        {
+              foundItems.push(response.data['menu_items'][i]);
+        }
+   }
 
    return foundItems;
  });
 
 };
 }
-
-
 
 })();
